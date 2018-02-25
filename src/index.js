66e0ae90131f55080f245203c39ea07c48854a80
u-vys <vn.vysotski@gmail.com>
2018-02-25 11:51:49 +0300

    love-triangle-impl

--- src/index.js
+++ src/index.js
@@ -3,5 +3,25 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+    var i, k, n, count = 0;
+    for (i = 0; i < preferences.length; i++) {
+        if (icValidLover(preferences, i)) {
+            k = preferences[i];
+            if (icValidLover(preferences, k - 1)) {
+                n = preferences[k - 1];
+                if (preferences[n - 1] === (i + 1)) {
+                    count++;
+                }
+            }
+        }
+    }
+    return count / 3;
+}
+
+function icValidLover(preferences = [], index) {
+    if (preferences[index] > 0 && preferences[index] <= preferences.length && preferences[index] !== index + 1) {
+        return true;
+    }
+    else return false;
+}
+
